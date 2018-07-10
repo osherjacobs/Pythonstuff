c6aa69f9f4b6e8e264c1ac65876f6f4f8809e0ee
O. Jacobs <osher.jacobs@gmail.com>
2018-07-11 01:22:36 +0300

    Update direnumerator.py

--- direnumerator.py
+++ direnumerator.py
@@ -1,11 +1,11 @@
 # Code by OJ 10-07-2018
 
 ####################################################################################################################################
-# This script will inventarize the contents of a hard drive (changeable at 'tup_curdir = os.walk('X:\\'))'                         #
+# This script will inventarize the contents of a hard drive (changeable at 'tup_curdir = os.walk('i:\\'))'                         #
 # and write the file / dir names to a txt file and then dump the contents into an Excel file (using openpyxl) whereafter a set of  #
 # hyperlinks can be created to the files on disk.                                                                                  #
 #                                                                                                                                  #
-# A little formatting and one copy action is necessary before creating hyperlinks with the =Hyperlink(a1;b1)Excel function.        #
+# A little formatting and one copy action is necessary before creating hyperlinks with the =Hyperlink(a1;b1)                       #
 #                                                                                                                                  #
 # The necessary Excel function for said formatting is detailed below for the Dutch language version I use.                         #
 ####################################################################################################################################
@@ -14,9 +14,11 @@ import os
 from openpyxl import Workbook
 os.chdir('n:\\')  # should be changed to another drive - will hold output files [1 text and one Excel]
 
-tup_curdir = os.walk('X:\\')  # this is the drive we want to inventorize.
+tup_curdir = os.walk('m:\\')  # this is the drive we want to inventorize.
 
-with open('dirsN.txt', 'w', encoding='utf-8') as fin:
+file_name = 'dirsN.txt' # tmp holder for data to be copied to Excel / gets deleted at end.
+
+with open(file_name, 'w', encoding='utf-8') as fin:
     counter = 0
     for p, d, f in tup_curdir:
         for file in f:
@@ -36,12 +38,13 @@ def new_excel():
         for y, i in enumerate(fout):
             if y == 0:
                 y += 1
-            #Write data to sheet    
             ws1.cell(row=y, column=1).value = i.strip('"')  # debatable if .strip() is necessary
 
-    wb.save('fess.xlsx') # change to desired name
+    wb.save('fess.xlsx')  # change to desired name
 
 
 new_excel()
+
+os.remove(file_name) # comment/uncomment out as desired.
 
 # To format out quotation marks for creation of hyperlinks in Excel run = WISSEN.CONTROL(cell) and copy down Eng = Clean; you need two columns anyway for hyperlink+descript.
