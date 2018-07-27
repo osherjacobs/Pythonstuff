6409c53e308907f313cf79d0ed09bbfa146767e3
O. Jacobs <osher.jacobs@gmail.com>
2018-07-27 14:07:05 +0300

    Update localtemperatureAPIquery.py

--- localtemperatureAPIquery.py
+++ localtemperatureAPIquery.py
@@ -2,7 +2,11 @@ import requests
 import json
 import owmcreds
 import pprint
+import time
+import pyfiglet
 
+heading = pyfiglet.figlet_format("OJ's Gratuitous Weather App")
+print(heading)
 weather_api_url = 'https://api.openweathermap.org/data/2.5/weather?'
 
 payload_2 = {'q': 'CITY_NAME', 'units': 'metric', 'APPID': owmcreds.key}
@@ -10,22 +14,12 @@ response_city_name = requests.get(weather_api_url, params=payload_2)
 
 i = json.loads(response_city_name.content)
 
-# print(response_city_name.content)
-
 print('The temperature in {} ({}) is currently {} degrees Celsius'.format(i['name'], i['sys']['country'], i['main']['temp']))
 
-print('\n\nFull JSON output from API is: ')
-pprint.pprint(f'{i}')
+sunset = i['sys']['sunset']
+sundown = time.localtime(sunset)
+print('Sunset is at {:2d}:{:2d}:{:02d} today ({}/{}/{})'.format(sundown.tm_hour, sundown.tm_min, sundown.tm_sec, sundown.tm_mday, sundown.tm_mon, sundown.tm_year))
+print('\n')
+print(f'Full JSON output of API from {weather_api_url[:-1]}: ')
 
-print(f'All information from {weather_api_url[:-1]}')
-
-#payload = {'lat': 'xx.xx', 'lon': 'xx.xx', 'units': 'metric', 'APPID': owmcreds.key}
-
-#response = requests.get(weather_api_url, params=payload)
-
-# if response.status_code == 200:
-
-#     print(response.content)
-# else:
-#     print('Error message {}'.format(response.status_code))
-# i = json.loads(response.content)
+pprint.pprint(i)
