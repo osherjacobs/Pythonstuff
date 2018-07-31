c48d4bef92b7636be558ec2e6c6370ba3baa936f
O. Jacobs <osher.jacobs@gmail.com>
2018-07-31 20:47:26 +0300

    Update backup_test1_rmvold.py

--- backup_test1_rmvold.py
+++ backup_test1_rmvold.py
@@ -5,7 +5,8 @@ import os.path
 import shutil
 import stat
 now = time.time()
-old = now - (24 * 60 * 60)
+import send2trash
+old = now - (7 * (24 * 60 * 60))
 time_now = int(time.time())
 time_now = str(time_now)
 time_limit = datetime.timedelta(days=1)
@@ -31,20 +32,19 @@ def remove_old_backups(location):
     path = location
     for f in os.listdir(path):
         if 'DTOPBACKUP' in f:  # run dir command from console : Windows Explorer lies and hides.....
-            if os.stat(f).st_mtime < now:
-                os.chmod(f, stat.S_IWRITE)
-                shutil.rmtree(f)
+            if os.stat(f).st_mtime < now - (60):
                 with open('log_removed_backups.txt', 'a', encoding='utf-8') as fd:
                     fd.write('Removed {} at {}'.format(f, datetime.datetime.now()))
                     fd.write('\n')
 
-                print('REMOVING {}'.format(f))
+                    #os.chmod(f, stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH | stat.S_IXUSR | stat.S_IRUSR | stat.S_IWUSR | stat.S_IWGRP | stat.S_IXGRP)
+                    send2trash.send2trash(f)#shutil.rmtree gives permission errors
+                    #print('REMOVED {}'.format(f))
 
 
-
-# backup()
-# time.sleep(10)
-# backup_of_backup()
-# time.sleep(5)
-remove_old_backups('E:\\')
-# remove_old_backups('E:\\')
+backup()
+time.sleep(10)
+backup_of_backup()
+time.sleep(5)
+remove_old_backups('E:\\BCKPOFBCKP')
+remove_old_backups('N:\\')
