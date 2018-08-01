6ed58fe4219d4c216a853eff343322c0eaa2e573
O. Jacobs <osher.jacobs@gmail.com>
2018-08-01 22:42:49 +0300

    Update epoch_time_to_string_snippet.py

--- epoch_time_to_string_snippet.py
+++ epoch_time_to_string_snippet.py
@@ -2,5 +2,6 @@ for file in i:
     stamp_file = (os.stat(file).st_mtime)
     stamp_file = int(stamp_file)
     stamp_file = time.gmtime(stamp_file)
+    fmt = "%d-%m-%Y %H:%M:%S"
     timobj = time.strftime(fmt, stamp_file)
     print(file, timobj)
