882a9535a7522a64cf438a3d5413a570fa2c7dbe
O. Jacobs <osher.jacobs@gmail.com>
2018-08-01 23:03:50 +0300

    Update epoch_time_to_string_snippet.py

--- epoch_time_to_string_snippet.py
+++ epoch_time_to_string_snippet.py
@@ -1,3 +1,5 @@
+
+
 for file in i:
     stamp_file = (os.stat(file).st_mtime)
     stamp_file = int(stamp_file)
@@ -5,3 +7,19 @@ for file in i:
     fmt = "%d-%m-%Y %H:%M:%S"
     timobj = time.strftime(fmt, stamp_file)
     print(file, timobj)
+
+# Recursive 'os.walk(DRIVE)' version:
+
+for p,d,f in os.walk('M:\\'):
+	for file in f:
+	        file = os.path.join(p,file)
+	        stamp_file = (os.stat(file).st_mtime)
+	        stamp_file = int(stamp_file)
+	        stamp_file = time.gmtime(stamp_file)
+	        timobj = time.strftime(fmt, stamp_file)
+	        
+	        with open('file_details.txt', 'a', encoding='utf-8') as fd:
+	            fd.write(file )
+	            fd.write("  TIMESTAMP: ")
+	            fd.write(timobj)
+	            fd.write('\n')
